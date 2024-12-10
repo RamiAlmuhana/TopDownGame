278c5ec2404558804abe97cfcc22ee0fa1bd628e
RamiAlmuhana <ramealmuhana@hotmail.com>
2024-12-11 00:44:18 +0100

    Character draait welke kant je loopt

--- Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float _speed;
     
+    [SerializeField]
+    private float _rotationSpeed;
+    
     private Rigidbody2D _rigidbody2D;
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
@@ -18,12 +21,29 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        SetPlaterVelocity();
+        RotateDirectionOfInput();
+    }
+
+    private void SetPlaterVelocity()
+    {
         _smoothedMovementInput = Vector2.SmoothDamp(
             _smoothedMovementInput,
             _movementInput,
             ref _movementInputSmoothVelocity,
             0.1f);
         _rigidbody2D.linearVelocity = _smoothedMovementInput * _speed;
+    }
+
+    private void RotateDirectionOfInput()
+    {
+        if (_movementInput != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _smoothedMovementInput);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+            
+            _rigidbody2D.MoveRotation(rotation);
+        }
     }
 
     private void OnMove(InputValue inputValue)
